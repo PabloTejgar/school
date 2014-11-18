9ede7a3d23f189b4845388907967d918a44e3bef
Pablo <pablo@pabloCorp.es>
2014-11-18 10:56:02 +0100

    Cosas de mi branch 1

--- schoolProject/main.cpp
+++ schoolProject/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     qDebug() << "Probando las maravillas de GitHUB";
+    qDebug() << "Estos son cosas de mis brances chulos";
 
     return a.exec();
 }
