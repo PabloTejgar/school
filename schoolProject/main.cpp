d7348dd57fd07514016d1d4a7b0c9d05b401785d
Pablo <pablo@pabloCorp.es>
2014-11-18 10:53:47 +0100

    Más

--- schoolProject/main.cpp
+++ schoolProject/main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    qDebug() << "Probando las maravillas de GitHUB";
 
     return a.exec();
 }
